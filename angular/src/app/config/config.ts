06aaf5732daace4f5e2a688615026bf7e24115e4
JB Ladera <minidomou@gmail.com>
2026-01-19 15:42:17 -0600

    finish angular

--- angular/src/app/config/config.ts
+++ angular/src/app/config/config.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { environment } from '../environments/environment';
+import { environment } from '../../environments/environment';
 import { JsonPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
