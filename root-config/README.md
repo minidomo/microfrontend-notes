7affe5b2a81a4448b5af33dc29e7fc490cdb16ba
JB Ladera <minidomou@gmail.com>
2026-01-19 17:13:48 -0600

    testing script

--- root-config/README.md
+++
@@ -1,4 +0,0 @@
-
-```sh
-create-single-spa --dir root-config --moduleType root-config --skipInstall --layout --typescript --packageManager npm --orgName project
-```
--- setup-root.sh
+++ setup-root.sh
@@ -6,6 +6,23 @@ if [ -n "$1" ]; then
     target_dir="$(cd "$1" 2>/dev/null && pwd)"
 fi
 
-rm -rf "$target_dir/.husky" "$target_dir/.git"
-sed -i '/husky/d' "$target_dir/package.json"
-sed -i '/pretty-quick/d' "$target_dir/package.json"
+(
+    cd "$target_dir"
+    rm -rf .husky .git
+    sed -i '/husky/d' package.json
+    sed -i '/pretty-quick/d' package.json
+
+    npm update --save
+    npm i single-spa@latest single-spa-layout@latest
+    npm i @types/node@22 dotenv -D
+
+    touch .env .env.example .env.production
+
+    sed -i '/disableHtmlGeneration/a\
+outputSystemJS: true,
+' webpack.config.js
+
+    set -i '//i\
+    
+' webpack.config.js
+)
