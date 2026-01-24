4c155e173135bf800b511e6bb5fb5726e1ad1f7d
JB Ladera <minidomou@gmail.com>
2026-01-23 21:21:15 -0600

    add more customization to nginx and dockerfile

---
+++ root-config/apply-env-templates.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# obtains only the environment variables that start with a given prefix and formats it
+# to string matching: "$PREFIX_<name1>,$PREFIX_<name2>,..."
+prefix="${CONFIG_PREFIX}"
+file_regex="${CONFIG_FILE_REGEX}"
+root_dir="${CONFIG_ROOT_DIR:-/usr/share/nginx/html}"
+
+target_env_vars=$(printenv | awk -F= '{print $1}' | grep "^$prefix" | sed 's/^/\$/g' | paste -sd,)
+
+if [ -n "$target_env_vars" ]; then
+    (
+        cd "${root_dir}"
+
+        echo "Available files in ${root_dir}:"
+        find . -type f
+        
+        files=$(find . -type f -regex "${file_regex}")
+
+        if [ -z "$files" ]; then
+            echo "Did not find any files with the regex: ${file_regex}"
+            exit 0
+        fi
+
+        for file in $files; do
+            echo "Matched ${file}"
+            
+            tmp=$(mktemp)
+            chmod "$(stat -c "%a" $file)" $tmp
+            envsubst $target_env_vars < "$file" > "$tmp"
+            mv "$tmp" "$file"
+        done
+    )
+fi
