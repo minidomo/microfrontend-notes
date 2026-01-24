27cdfba3eae5592f17c34ecab64571c0f8e41701
JB Ladera <minidomou@gmail.com>
2026-01-23 23:11:14 -0600

    update docker and nginx configs (#5)
    * add more customization to nginx and dockerfile
    
    * set cache
    
    * add 404 on missing index and update js with CORS
    
    * add initial check for empty prefix and file regex
    
    * add .env file
    
    * update mfes with new docker config
    
    * remove individual compose files
    
    * remove depends on for root

---
+++ angular/apply-env-templates.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+prefix="${CONFIG_ENV_PREFIX}"
+file_regex="${CONFIG_FILE_REGEX}"
+root_dir="${CONFIG_ROOT_DIR}"
+
+if [ -z "$prefix" ] || [ -z "$file_regex" ]; then
+    echo "CONFIG_ENV_PREFIX and/or CONFIG_FILE_REGEX are not set. Skipping substitution."
+    exit 0
+fi
+
+# obtains only the environment variables that start with a given prefix and formats it
+# to string matching: "$PREFIX_<name1>,$PREFIX_<name2>,..."
+target_env_vars=$(printenv | awk -F= '{print $1}' | grep "^$prefix" | sed 's/^/\$/g' | paste -sd,)
+
+if [ -n "$target_env_vars" ]; then
+    (
+        cd "${root_dir}"
+
+        echo "Available files in ${root_dir}:"
+        find . -type f
+        
+        files=$(find . -type f -regex "${file_regex}")
+
+        if [ -z "$files" ]; then
+            echo "Did not find any files with the regex: ${file_regex}"
+            exit 0
+        fi
+
+        for file in $files; do
+            echo "Matched ${file}"
+
+            tmp=$(mktemp)
+            chmod "$(stat -c "%a" $file)" $tmp
+            envsubst $target_env_vars < "$file" > "$tmp"
+            mv "$tmp" "$file"
+        done
+    )
+fi
