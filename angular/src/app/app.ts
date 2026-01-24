bae2aef41e3fee78296c4591e457cf64a437434e
JB Ladera <minidomou@gmail.com>
2026-01-24 01:23:23 -0600

    adjust css encapsulation to be global and move tailwind import to app (#7)

--- angular/src/app/app.ts
+++ angular/src/app/app.ts
@@ -1,10 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
+  styleUrl: './app.css',
+  encapsulation: ViewEncapsulation.None,
 })
 export class App {
   protected readonly title = signal('angular');
